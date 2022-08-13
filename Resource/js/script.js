74640c6e1ae265efbc13ff0b9a30303abd3af5a1
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-08-13 22:14:37 +0700

    Feat: done desktop version

--- Resource/js/script.js
+++ Resource/js/script.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
     const body = document.querySelector("body"),
-         modeToggle = body.querySelector(".mode-toggle");
+         modeToggle = body.querySelector(".mode-toggle"),
+         sidebar = body.querySelector("nav"),
+         sidebarToggle = body.querySelector(".sidebar-toggle");
 
+    //Switch between dark and light theme
     modeToggle.addEventListener("click", function(){
-        body.classList.toggle("dark")
+        body.classList.toggle("dark");
     });
+
+    //Display and hide menu bar
+    sidebarToggle.addEventListener("click", function(){
+        sidebar.classList.toggle("close");
+    });
+
+    //Data table for tbl_cart
+    $('#tbl_cart').DataTable( {
+        "pageLength": 100,
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
 })
