06b5361c9fbbf5b74bbc78fdcb830b4ebeb19e18
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-08-13 07:57:12 +0700

    Feat: done switch mode

--- Resource/js/script.js
+++ Resource/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    const body = document.querySelector("body"),
+         modeToggle = body.querySelector(".mode-toggle");
+
+    modeToggle.addEventListener("click", function(){
+        body.classList.toggle("dark")
+    });
 })
